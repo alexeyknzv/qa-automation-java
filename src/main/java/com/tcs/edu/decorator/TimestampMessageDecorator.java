44dbd495747f29ab379df7cb9862086305320136
a.knyazev <a.knyazev@tinkoff.ru>
2022-03-27 21:32:23 +0700

    refactor: changed structure to packages feature: decoration class added docs: javadoc comments added

---
+++ src/main/java/com/tcs/edu/decorator/TimestampMessageDecorator.java
@@ -0,0 +1,20 @@
+package com.tcs.edu.decorator;
+
+import java.time.Instant;
+
+/**
+ * Class for adding a timestamp to a string
+ * @author Alexey Knyazev
+ */
+
+public class TimestampMessageDecorator {
+    /**
+     * Adds timestamp with current date-time
+     * @param message string to add timestamp
+     * @return given string with timestamp in the beginning
+     */
+    public static String decorate(String message) {
+
+        return Instant.now() + " " + message;
+    }
+}
