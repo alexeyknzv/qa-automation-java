8c9793c7ca8e8edc4bf3a1a77c18fcde40fb8bab
a.knyazev <a.knyazev@tinkoff.ru>
2022-04-04 00:08:27 +0700

    feat: index number for print added

--- src/main/java/com/tcs/edu/decorator/TimestampMessageDecorator.java
+++ src/main/java/com/tcs/edu/decorator/TimestampMessageDecorator.java
@@ -3,19 +3,20 @@ package com.tcs.edu.decorator;
 import java.time.Instant;
 
 /**
- * Class for adding a timestamp to a string
+ * Class for adding an index number and a timestamp to a string
  * @author Alexey Knyazev
  */
 
 public class TimestampMessageDecorator {
+    public static int messageCount = 1;
     /**
-     * Adds timestamp with current date-time
+     * Adds index number and timestamp with current date-time
      * @param message string to add timestamp
-     * @return given string with timestamp in the beginning
+     * @return given string with index number and timestamp in the beginning
      */
     public static String decorate(String message) {
 
-        String decoratedMessage = Instant.now() + " " + message;
+        String decoratedMessage = messageCount + " " + Instant.now() + " " + message;
         return decoratedMessage;
     }
 }
